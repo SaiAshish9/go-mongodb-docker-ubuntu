4706051a314e5e8d811da2c1d69228bf600ca46e
saiashish9 <saiashish3760@gmail.com>
2021-01-25 22:51:12 +0530

    mongodb config updated

---
+++ mongodb/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/saiashish9/go-mongodb-docker/mongodb/controllers"
+	"gopkg.in/mgo.v2"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8081", r)
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost")
+
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
